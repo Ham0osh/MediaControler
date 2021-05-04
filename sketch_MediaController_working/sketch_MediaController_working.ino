4e55f41641809de153ab417c539e5bb3db1b5a51
Ham0osh <67180838+Ham0osh@users.noreply.github.com>
2021-05-04 16:15:11 -0700

    Added more colour functionality.
    Mute toggles solid red. Volume adjustment colours swap back to standby colour after N milliseconds.

--- sketch_MediaController_working/sketch_MediaController_working.ino
+++ sketch_MediaController_working/sketch_MediaController_working.ino
@@ -31,15 +31,28 @@ int buttonStateL = 0;
 int buttonStateR = 0;
 int last_buttonStateL = 0;
 int last_buttonStateR = 0;
+unsigned long timeSince;
+unsigned long timeLast;
+unsigned long timeTrail;
+bool colIsDefault;
+bool colChanged;
+bool muted;
 
 void setup() {
   // put your setup code here, to run once:
   pixels.begin();
+   
+  timeSince = 0;
+  timeLast = millis();
+  timeTrail = 3000;
+  colIsDefault = true;
+  colChanged = false;
+  muted = false;
 
- for(int i=0; i<NUMPIXELS; i++) {
-   pixels.setPixelColor(i, pixels.Color(127, 0, 155));
-   pixels.show();
- }
+  for(int i=0; i<NUMPIXELS; i++) {
+    pixels.setPixelColor(i, pixels.Color(127, 0, 155));
+    pixels.show();
+  }
   
   rotary.setDebounceDelay(debounceDelay);
   rotary.setErrorDelay(errorDelay);
@@ -63,11 +76,13 @@ void loop() {
   if ( buttonStateL == 0 && last_buttonStateL == 1 ) {
     Serial.println("LEFT");
     Consumer.write(MEDIA_PREVIOUS);
+    colIsDefault = false;
   }
   // RightButton
   if ( buttonStateR == 0 && last_buttonStateR == 1 ) {
     Serial.println("RIGHT");
     Consumer.write(MEDIA_NEXT);
+    colIsDefault = false;
   }
  
   // CW
@@ -77,8 +92,11 @@ void loop() {
     // Do something cool
     for(int i=0; i<NUMPIXELS; i++) {
       pixels.setPixelColor(i, pixels.Color(255, 128, 0));
-      pixels.show();
     }
+    colChanged = true;
+    timeLast = millis();
+    colIsDefault = false;
+    muted = false;
   }  
   
   // CCW
@@ -87,9 +105,12 @@ void loop() {
     Consumer.write(MEDIA_VOLUME_DOWN);
     for(int i=0; i<NUMPIXELS; i++) {
       pixels.setPixelColor(i, pixels.Color(0, 255, 255));
-      pixels.show();
     // Do something cool
     }
+    colChanged = true;
+    timeLast = millis();
+    colIsDefault = false;
+    muted = false;
   }
   
 
@@ -98,6 +119,8 @@ void loop() {
     // Do something cool.
     Serial.println("Short Push");
     Consumer.write(MEDIA_PLAY_PAUSE);
+    timeLast = millis();
+    colIsDefault = false;
   }
   
   //Long push
@@ -105,8 +128,38 @@ void loop() {
     // Do something cool.
     Serial.println("Long Push");
     Consumer.write(MEDIA_VOLUME_MUTE);
+    timeLast = millis();
+    colIsDefault = false;
+    muted = not(muted);
+    if (not(muted)){
+      for(int i=0; i<NUMPIXELS; i++) {
+        pixels.setPixelColor(i, pixels.Color(127, 0, 155));
+      }
+      colChanged = true;
+      colIsDefault = true;
+    }
   }
   delay(10);
   last_buttonStateL = buttonStateL;
   last_buttonStateR = buttonStateR;
+
+  if (  not(colIsDefault) && not(muted) && millis()-timeLast >= timeTrail ){
+    for(int i=0; i<NUMPIXELS; i++) {
+      pixels.setPixelColor(i, pixels.Color(127, 0, 155));
+    }
+    colChanged = true;
+    colIsDefault = true;
+  }
+
+  if (muted){
+    for(int i=0; i<NUMPIXELS; i++) {
+      pixels.setPixelColor(i, pixels.Color(255, 0, 15));
+    }
+    colChanged = true;
+  }
+  if (colChanged){
+    pixels.show();
+  }
+  
+  colChanged = false;
 }
